c1e604bef5110c58b7ed1f54053dbb1f42acce72
wyaoyao <wangyaoyao92@163.com>
2020-12-22 20:53:06 +0800

    gateway 入门

---
+++ cloud-gateway/src/main/java/study/wyy/cloud/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package study.wyy.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author by wyaoyao
+ * @Description
+ * @Date 2020/12/22 7:14 下午
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
