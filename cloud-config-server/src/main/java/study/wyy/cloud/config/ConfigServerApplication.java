d0ab1ff3841b223d890d6998eaf5d84084f1e313
wyaoyao <wangyaoyao93@163.com>
2020-12-23 07:43:21 +0800

    配置中心开始

---
+++ cloud-config-server/src/main/java/study/wyy/cloud/config/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package study.wyy.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author by wyaoyao
+ * @Description
+ * @Date 2020/12/22 10:26 下午
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
